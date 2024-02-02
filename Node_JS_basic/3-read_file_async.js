53655b413ff6f4344b84c5fcce77f34e55db2859
nadeshda18 <nadeshda02@hotmail.com>
2024-02-02 16:56:04 -0400

    update

--- Node_JS_basic/3-read_file_async.js
+++ Node_JS_basic/3-read_file_async.js
@@ -1,33 +1,50 @@
-/* eslint-disable */
-const fs = require('fs').promises;
+const fs = require('fs');
 
 function countStudents(path) {
-  return fs.readFile(path, 'utf8')
-    .then((data) => {
-      const lines = data.split('\n');
-      const students = lines.filter((line) => line).map((line) => line.split(','));
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (err) {
+        reject(Error('Cannot load the database'));
+        return;
+      }
+      const response = [];
+      let msg;
 
-      const num_students = students.length ? students.length - 1 : 0;
-      console.log(`Number of students: ${num_students}`);
+      const content = data.toString().split('\n');
+
+      let students = content.filter((item) => item);
+
+      students = students.map((item) => item.split(','));
+
+      const NUMBER_OF_STUDENTS = students.length ? students.length - 1 : 0;
+      msg = `Number of students: ${NUMBER_OF_STUDENTS}`;
+      console.log(msg);
+
+      response.push(msg);
 
       const fields = {};
-      for (const student of students) {
-        const field = student[3];
-        if (!fields[field]) {
-          fields[field] = [];
+      for (const i in students) {
+        if (i !== 0) {
+          if (!fields[students[i][3]]) fields[students[i][3]] = [];
+
+          fields[students[i][3]].push(students[i][0]);
         }
-        fields[field].push(student[0]);
       }
 
-        delete fields.field;
+      delete fields.field;
 
-      for (const field in fields) {
-        console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
+      for (const key of Object.keys(fields)) {
+        msg = `Number of students in ${key}: ${
+          fields[key].length
+        }. List: ${fields[key].join(', ')}`;
+
+        console.log(msg);
+
+        response.push(msg);
       }
-    })
-    .catch(() => {
-      throw new Error('Cannot load the database');
+      resolve(response);
     });
+  });
 }
 
 module.exports = countStudents;
